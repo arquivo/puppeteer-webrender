62323281ae645f8b4d61f112f4d0146c4387601f
Daniel Bicho <daniel.bicho@fccn.pt>
2019-11-25 18:18:24 +0000

    let express.js handle the error for now..

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ async function renderScreenshot(url) {
 
     await page.goto(url, {waitUntil: 'load', timeout: 10000}).then(() =>{
         console.log('Success loading page', url)
-    }).catch((res) => {
-        console.log('Something went wrong loading page', url, res)
+    }).catch((err) => {
+        console.log('Something went wrong loading page', url, err)
         browser.close()
-        throw res
+        return err;
     });
 
     let result = await page.screenshot({type: 'jpeg', fullPage: true});
@@ -46,19 +46,12 @@ async function renderScreenshot(url) {
     return result;
 }
 
-app.get('/screenshot', (request, response) => {
+app.get('/screenshot', (request, response, next) => {
     // render?url=http....
     console.log("Rendering screenshot for: ", request.query.url);
-    renderScreenshot(request.query.url).then( res => {
-        if (res){
-            response.set('Content-Type', 'image/jpeg').send(res)
-        }
-        else {
-            response.status(500);
-            response.send("Something went wrong rendering the page!!")
-        }
-        }
-    );
+    renderScreenshot(request.query.url)
+        .then((res) => response.set('Content-Type', 'image/jpeg').send(res))
+        .catch(error => next(error))
 });
 
 async function generateHar(url){
@@ -94,15 +87,9 @@ async function generateHar(url){
 
 app.get('/har', (request, response) => {
     console.log("Rendering page and generating HAR...")
-    generateHar(request.query.url).then(res => {
-        if (res){
-            response.set('Content-Type', 'application/json').send(res)
-        }
-        else {
-            response.status(500);
-            response.send("Something went wrong rendering the page!!")
-        }
-    });
+    generateHar(request.query.url)
+        .then(res => response.set('Content-Type', 'application/json').send(res))
+        .catch((error) => next(error))
 });
 
 
