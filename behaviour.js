bdd43ffbfac10818a7fa41aaef04bff040f27b2e
Daniel Bicho <daniel.bicho@fccn.pt>
2019-11-25 18:42:11 +0000

    split behaviours

---
+++ behaviour.js
@@ -0,0 +1,25 @@
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight || totalHeight > 4000) {
+                    clearInterval(timer);
+                    // Scroll back to the top:
+                    window.scrollTo(0, 0);
+                    resolve();
+                }
+            }, 200);
+        });
+    });
+}
+
+
+module.exports = {
+    autoScroll: this.autoScroll
+};
