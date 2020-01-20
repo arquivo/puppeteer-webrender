1c16a0e205d693477950e535338c61d607f53baf
Daniel Bicho <daniel.bicho@fccn.pt>
2020-01-20 17:10:08 +0000

    Update patching functionality pass options through command-line. Specify number of browsers, tab-pages, file with urls and timeout.

--- patching.js
+++ patching.js
@@ -1,18 +1,55 @@
 const puppeteer = require('puppeteer');
 const behaviour = require('./behaviour');
 const fs = require('fs');
-
-const num_browsers = 2;
-const num_pages = 3;
+const yargs = require('yargs');
 
 function readURLFile(path) {
-    return fs.readFileSync(path, 'utf-8').split('\n');
+    return fs.readFileSync(path).toString().split('\n');
 }
+
+const argv = yargs
+    .usage('$0 <cmd> [args]')
+    .command('patch', 'Launch puppeteer to patch arquivo.pt webarchives.', (yargs) => {
+        yargs.option('num_browsers', {
+            description: 'The number of browsers instances to use.',
+            alias: 'b',
+            type: 'number',
+            default: 1
+        })
+        .option('num_pages', {
+            description: 'The number of tabs pages for each browser instance top use.',
+            alias: 'p',
+            type: 'number',
+            default: 1
+        })
+        .option('urls_list', {
+            description: 'List of urls to patch.',
+            alias: 'i',
+            type: 'string',
+            default: 'urls.txt'
+        })
+        .option( 'timeout', {
+            description: 'browsing page timeout',
+            alias: 't',
+            type: 'number',
+            default: 90000
+        })
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+const num_browsers = argv.num_browsers
+const num_pages = argv.num_pages
+const urls_list = argv.urls_list;
+const timeout = argv.timeout;
+
+console.log("Lauching patching with %i browsers and %i page tabs.", num_browsers, num_pages);
 
 (async () => {
     const initialTime = Date.now();
-    console.log(process.argv[2])
-    const urls = readURLFile(process.argv[2]);
+    const urls = readURLFile(urls_list);
 
     const promisesBrowsers = [];
 
@@ -25,9 +62,10 @@ function readURLFile(path) {
                 promisesPages.push(new Promise(async (resPage) => {
                     while(urls.length > 0){
                         const url = urls.pop();
-                        console.log("Patching URL", url);
+                        if (url != ''){
+                         console.log("Patching URL", url);
                         let page = await browser.newPage();
-                        page.setDefaultNavigationTimeout(90000);
+                        page.setDefaultNavigationTimeout(timeout);
                         let before = Date.now();
                         try{
                             await page.goto(url, 'networkidle2');
@@ -41,8 +79,8 @@ function readURLFile(path) {
                         } finally {
                             await page.close();
                         }
+                        }
                     }
-
                     resPage()
                 }));
             }
