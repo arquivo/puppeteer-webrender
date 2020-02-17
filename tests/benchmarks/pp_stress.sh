e95c47784ad89c1a22d816c291834a56348a38e9
Daniel Bicho <daniel.bicho@fccn.pt>
2020-02-17 16:09:33 +0000

    run inside docker containers with no root user

---
+++ tests/benchmarks/pp_stress.sh
@@ -0,0 +1,5 @@
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' "http://localhost:5000/screenshot?url=https://arquivo.pt/noFrame/replay/2019/https://sobre.arquivo.pt" &
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' "http://localhost:5000/screenshot?url=https://arquivo.pt/noFrame/replay/2019/https://sobre.arquivo.pt" &
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' "http://localhost:5000/screenshot?url=https://arquivo.pt/noFrame/replay/2019/https://sobre.arquivo.pt" &
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' "http://localhost:5000/screenshot?url=https://arquivo.pt/noFrame/replay/2019/https://sobre.arquivo.pt" &
+curl -o /dev/null -s -w 'Total: %{time_total}s\n' "http://localhost:5000/screenshot?url=https://arquivo.pt/noFrame/replay/2019/https://sobre.arquivo.pt" &
